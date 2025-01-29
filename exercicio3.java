ffed76153857f1f4a6aaddcc61b104f6d20be077
JoJose-Vitor <josevitorg@gmail.com>
2025-01-29 16:24:10 -0300

    alterando o arquivo readme.md

---
+++ exercicio3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o valor em centavos: ");
+        int valor = scanner.nextInt();
+
+        int centavos = valor % 100;
+        int reais = valor / 100;
+        System.out.println("O total em dinheiro eh: " + reais + " real(is) e " + centavos + " centavo(s)");
+
+    }
+}

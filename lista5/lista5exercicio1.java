486aa269bee3490d17c6857891e9cc1d42161cb9
JoJose-Vitor <josevitorg@gmail.com>
2025-01-30 17:05:56 -0300

    Feita a lista 5 e correção na lista 4 exercicio 1

---
+++ lista5/lista5exercicio1.java
@@ -0,0 +1,13 @@
+package lista5;
+
+public class lista5exercicio1 {
+    public static void main(String[] args){
+        int sobe = 0;
+        int desce = 200;
+        int i;
+
+        for (i = 0; i <=50; i++){
+            System.out.println((sobe + (2 * i)) + " - " + (desce - (4 * i)));
+        }
+    }
+}
--- lista5/lista5exercicio2.java
+++ lista5/lista5exercicio2.java
@@ -1,11 +1,12 @@
-package lista4;
-public class lista4exercicio1 {
+package lista5;
+
+public class lista5exercicio2 {
     public static void main(String[] args){
-        int fator1 = 1;
+        int fator1 = 10;
         int fator2 = 1;
         int valor;
 
-        while(fator1 <= 10){
+        while(fator1 >= 1){
             while(fator2 <= 10){
                 valor = fator1 * fator2;
                 System.out.println(fator1 + " X " + fator2 + " = " + valor);
@@ -13,7 +14,7 @@ public class lista4exercicio1 {
             }
             System.out.println("");
             fator2 = 1;
-            fator1++;
+            fator1--;
         }
     }
 }

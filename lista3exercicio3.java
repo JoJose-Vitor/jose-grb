6c6ed518042304410376fdb3b243d1fe42c6cec3
JoJose-Vitor <josevitorg@gmail.com>
2025-01-30 10:17:59 -0300

    adicionando lista 3 de exercicios

--- lista3exercicio3.java
+++ lista3exercicio3.java
@@ -1,23 +1,39 @@
 import java.util.Objects;
 import java.util.Scanner;
 
-public class lista2exercicio1 {
+public class lista3exercicio3 {
     public static void main(String[] args){
+
+        String usuario;
+        String senha;
+        int i = 1;
         
         Scanner scanner = new Scanner(System.in);
 
+        do{
+
         System.out.println("Informe seu usuário: ");
-        String usuario = scanner.nextLine();
+        usuario = scanner.nextLine();
         System.out.println("Informe sua senha: ");
-        String senha = scanner.nextLine();
+        senha = scanner.nextLine();
 
         Boolean ulogin = (Objects.equals(usuario, "admin"));
         boolean usenha = (Objects.equals(senha, "123senha"));
         if (ulogin == true && usenha == true){
             System.out.println("Login realizado com sucesso!");
+            break;
         }else{
-            System.out.println("Credenciais inválidas...");
+            System.out.println("Credenciais inválidas... tente novamente:");
+
+            i++;
+
         }
+        }while(i <= 3);
+            
+        if (i == 3){
+            System.out.println("Sua conta foi bloqueada");
+        
 
     }
+}
 }
